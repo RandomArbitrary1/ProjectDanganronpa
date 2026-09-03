3c189c472a264c7ad44a702cc2db1998c304f44e
Danilo <eggwardchickfila@gmail.com>
2026-09-03 22:18:07 +0200

    further progress with shooter

--- assets/scripts/class_trial/debate_roulette.gd
+++ assets/scripts/class_trial/debate_roulette.gd
@@ -3,10 +3,17 @@ extends Control
 @onready var big_gunshot: AudioStreamPlayer = $sfx/big_gunshot
 @onready var small_gunshot: AudioStreamPlayer = $sfx/small_gunshot
 @onready var break_sfx: AudioStreamPlayer = $sfx/break
+@onready var noise_shoot_vfx: TextureRect = $crosshair/noise_shoot_vfx
+@onready var noise_anim: AnimationPlayer = $crosshair/noise_shoot_vfx/noise_anim
+@onready var hp: ProgressBar = $HP
+@onready var concentrate: ProgressBar = $Concentrate
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hp.value = 100
+	concentrate.value = 100
+	noise_anim.play("hide")
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +25,6 @@ func _input(event: InputEvent) -> void:
 		white_noise_shoot()
 		
 func white_noise_shoot():
+	noise_anim.stop()
+	noise_anim.play("shoot")
 	small_gunshot.play()
