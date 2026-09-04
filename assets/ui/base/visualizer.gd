1594ec3ebd489aedc8407e79c2923114d4422e66
mkaiast <itsmkaiast@gmail.com>
2026-09-05 01:20:54 +0200

    boombox

--- assets/ui/base/visualizer.gd
+++ assets/ui/base/visualizer.gd
@@ -1,13 +1,9 @@
 extends Control
 
-var analyzer = AudioEffectSpectrumAnalyzerInstance
-
-func _ready() -> void:
-	pass
-	#analyzer = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("Music"), 0)
-	#print(analyzer)
+var bus = AudioServer.get_bus_index("Music")
 
 func _process(_delta: float) -> void:
-	pass
-	#analyzer = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("Music"), 0)
-	#print(analyzer.get_magnitude_for_frequency_range(20.0, 60.0))
+	var db = db_to_linear(max(AudioServer.get_bus_peak_volume_left_db(bus, 0), AudioServer.get_bus_peak_volume_right_db(bus, 0)))
+	var bounce = Vector2(.8,.8) + Vector2(db,db)/4
+	self.get_node("Speaker1").scale = bounce
+	self.get_node("Speaker2").scale = bounce
