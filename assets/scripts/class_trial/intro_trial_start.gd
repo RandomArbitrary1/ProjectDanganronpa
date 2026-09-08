551e030a26ce6b0ce01c7dc21034271ccc11f832
Danilo <eggwardchickfila@gmail.com>
2026-09-08 13:05:02 +0200

    small changes

--- assets/scripts/class_trial/intro_trial_start.gd
+++ assets/scripts/class_trial/intro_trial_start.gd
@@ -1,13 +1,17 @@
 extends Control
-@export var start_dialog_json:String
-
+@export_file_path("*.json") var start_dialog_json:String
+var hide_elms = false
+var started = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 func start():
-	pass
+	started = true
+	
+func hide_elements():
+	hide_elms = true
--- assets/scripts/class_trial/preparation_screen.gd
+++ assets/scripts/class_trial/preparation_screen.gd
@@ -1,7 +1,8 @@
 extends Control
-@onready var class_trial_main: Node3D = $".."
-@onready var revolver_cylinder: TextureRect = $revolver_cylinder
+@onready var class_trial_main: Node3D = $"..."
+@onready var revolver_cylinder: TextureRect = $pre_screen/revolver_cylinder
 @onready var intro: Control = $Intro
+@onready var pre_screen: Control = $pre_screen
 var timer = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	revolver_cylinder.rotation += delta * 0.3
+	
+	if intro.hide_elms:
+		pre_screen.visible = false
 	#class_trial_main.state = "debate"
 
 func _on_button_pressed() -> void:
