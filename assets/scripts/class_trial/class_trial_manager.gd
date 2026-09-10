57a32ab7c1b039e7ed787b1c795ca6bf740fe065
Danilo <eggwardchickfila@gmail.com>
2026-09-10 18:12:12 +0200

    asdf

--- assets/scripts/class_trial/class_trial_manager.gd
+++ assets/scripts/class_trial/class_trial_manager.gd
@@ -11,6 +11,7 @@ extends Node3D
 
 var state = "prepare"
 var data = JsonParse.load_json("class_trial/debate/debate1.json")
+var podiums = 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -54,6 +55,5 @@ func start_dialog():
 	dialog.start()
 	camera_3d.fov(30)
 func dialog_process(delta):
-	pass
-	#camera_3d.global_transform.looking_at(
-			#podium.global_position)
+	camera_3d.global_transform.looking_at(
+			podium.global_position)
