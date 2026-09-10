3c6633fdf20ac9c6ad8995626766d3107287ef74
mkaiast <itsmkaiast@gmail.com>
2026-09-10 15:54:47 +0200

    changes

--- assets/scripts/talking.gd
+++ assets/scripts/talking.gd
@@ -3,23 +3,14 @@ extends Node3D
 @onready var dialog = self.get_parent().get_node("UI/Dialog")
 @onready var label = self.get_parent().get_node("UI/Base/Hover_label")
 var character_info = preload("res://assets/data/characters/characters.json").data
+var camera = null
+
+var current_hover = null
+var current_hover_check = null
+const RAY_LENGTH = 1000
 
 func _ready() -> void:
-	for i in self.get_children():
-		i.get_node("Area3D").mouse_entered.connect(func():
-			if dialog.active == false:
-				label.get_node("Label").text = character_info[i.name].name
-				label.get_node("Anim").play("Open")
-		)
-		i.get_node("Area3D").mouse_exited.connect(func():
-			if dialog.active == false:
-				label.get_node("Anim").play("Close")
-		)
-		i.get_node("Area3D").input_event.connect(func(_a,event,_c,_d,_e):
-			if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-				label.get_node("Anim").play("Close")
-				run(i.name)
-		)
+	camera = get_tree().get_first_node_in_group("Camera_room")
 		
 func run(object):
 	if object == "raito":
@@ -28,3 +19,33 @@ func run(object):
 	elif object == "nori":
 		dialog.file = load("res://assets/data/dialog/test.json")
 		dialog.start()
+
+func _process(_delta: float) -> void:
+	if current_hover != null and current_hover_check != current_hover and dialog.active == false:
+		current_hover_check = current_hover
+		label.get_node("Label").text = character_info[current_hover.name].name
+		label.get_node("Anim").play("Open")
+	if dialog.active == false and current_hover == null:
+		current_hover_check = null
+		label.get_node("Anim").play("Close")
+	if current_hover and Input.is_action_just_pressed("Progress"):
+		var name_send = current_hover.name
+		current_hover = null
+		current_hover_check = null
+		label.get_node("Anim").play("Close")
+		run(name_send)
+
+func _physics_process(_delta):
+	var space_state = get_world_3d().direct_space_state
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = camera.project_ray_origin(mousepos)
+	var end = origin + camera.project_ray_normal(mousepos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	if result.has("collider") and result.collider.get_parent().get_parent() and result.collider.get_parent().get_parent().name == "Characters":
+		current_hover = result.collider.get_parent()
+	else:
+		current_hover = null
--- assets/scripts/ui/handbook.gd
+++ assets/scripts/ui/handbook.gd
@@ -5,6 +5,8 @@ extends ColorRect
 
 var open = false
 var tab = 0
+var inside_tab = 0
+@onready var current_tab_node = self.get_node("Screen/List")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("handbook"):
@@ -27,24 +29,41 @@ func _process(delta: float) -> void:
 					current.self_modulate.a = 0
 					current.get_node("Icon").modulate = Color(1,1,1)
 				
-	
-	if Input.is_action_just_pressed("Next"):
-		if tab < 5:
-			tab += 1
-		else:
-			tab = 0
-	if Input.is_action_just_pressed("Previous"):
-		if tab > 0:
-			tab -= 1
-		else:
-			tab = 5
-			
-	for i in tabs.get_child_count():
-		var current = tabs.get_child(i)
-		if i == tab:
-			current.self_modulate.a = lerp(current.self_modulate.a, 1.0, 20*delta)
-			current.get_node("Icon").modulate = current.get_node("Icon").modulate.lerp(Color(0.0,0.0,0.0), 20*delta)
-		else:
-			current.self_modulate.a = lerp(current.self_modulate.a, 0.0, 20*delta)
-			current.get_node("Icon").modulate = current.get_node("Icon").modulate.lerp(Color(1.0,1.0,1.0), 20*delta)
+	if get_tree().paused == true:
+		if Input.is_action_just_pressed("Next"):
+			if tab < 5:
+				tab += 1
+			else:
+				tab = 0
+		if Input.is_action_just_pressed("Previous"):
+			if tab > 0:
+				tab -= 1
+			else:
+				tab = 5
+				
+		for i in tabs.get_child_count():
+			var current = tabs.get_child(i)
+			if i == tab:
+				current.self_modulate.a = lerp(current.self_modulate.a, 1.0, 20*delta)
+				current.get_node("Icon").modulate = current.get_node("Icon").modulate.lerp(Color(0.0,0.0,0.0), 20*delta)
+			else:
+				current.self_modulate.a = lerp(current.self_modulate.a, 0.0, 20*delta)
+				current.get_node("Icon").modulate = current.get_node("Icon").modulate.lerp(Color(1.0,1.0,1.0), 20*delta)
 		
+		if Input.is_action_just_pressed("Left"):
+			if inside_tab > 0:
+				inside_tab -= 1
+			else:
+				inside_tab = current_tab_node.get_node("Items").get_child_count()-1
+		if Input.is_action_just_pressed("Right"):
+			if inside_tab < current_tab_node.get_node("Items").get_child_count()-1:
+				inside_tab += 1
+			else:
+				inside_tab = 0
+				
+		
+		for i in current_tab_node.get_node("Items").get_children():
+			if i.get_index(0) == inside_tab:
+				i.get_node("Outline").modulate.a = lerp(i.get_node("Outline").modulate.a, 1.0, 20*delta)
+			else:
+				i.get_node("Outline").modulate.a = lerp(i.get_node("Outline").modulate.a, 0.0, 20*delta)
