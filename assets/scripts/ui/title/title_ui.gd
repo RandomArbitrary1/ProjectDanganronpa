63bfc1ebba38e5392d41cc2e662ff941da078a16
Danilo <eggwardchickfila@gmail.com>
2026-09-12 00:45:43 +0200

    title screen rough 2

--- assets/scripts/ui/title/title_ui.gd
+++ assets/scripts/ui/title/title_ui.gd
@@ -1,5 +1,7 @@
 extends Control
 @onready var revolver: TextureRect = $revolver
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var confirm_sfx: AudioStreamPlayer = $confirm_sfx
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	revolver.rotation += delta
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed or event is InputEventMouseButton:
+		anim.play("press_any")
+		confirm_sfx.play()
