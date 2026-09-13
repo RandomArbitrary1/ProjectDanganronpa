f3508532b65d367336ade2d2262a43a5ebd3a18a
Danilo <eggwardchickfila@gmail.com>
2026-09-13 15:42:05 +0200

    loading screen updated

--- assets/scripts/ui/loading_screen/loading_screen.gd
+++ assets/scripts/ui/loading_screen/loading_screen.gd
@@ -11,6 +11,7 @@ var progress = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
+	animation_player.play("appear")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
