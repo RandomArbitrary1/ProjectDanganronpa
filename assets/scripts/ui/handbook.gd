cfb8ea4d67e1577daa55aade79cc1515fe74ac13
mkaiast <itsmkaiast@gmail.com>
2026-09-15 11:54:41 +0200

    yes

--- assets/scripts/ui/handbook.gd
+++ assets/scripts/ui/handbook.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends TextureRect
 
 @onready var anim : AnimationPlayer = $Anim
 @onready var tabs = $Screen/Tabs/List
@@ -9,11 +9,20 @@ var open = false
 var tab = 0
 var inside_tab = Vector2.ZERO
 @onready var current_tab_node = lists.get_node("System")
+var tab_data = []
 
 var x_range = 3
 var y_range = 2
 
+func _ready() -> void:
+	for i in lists.get_children():
+		var size_y = 1
+		if i.get_node("Items").get_child_count() > 0:
+			size_y = i.get_node("Items").get_child(1).custom_minimum_size.y
+		tab_data.append({"offset":i.get_node("Items").position.y,"size":size_y+4})
+
 func _process(delta: float) -> void:
+	self.self_modulate = self.self_modulate.lerp(tabs.get_child(tab).self_modulate*Color(.5,.5,.5), 20*delta)
 	if Input.is_action_just_pressed("handbook"):
 		if open:
 			Input.mouse_mode = original_mouse_mode
@@ -96,6 +105,9 @@ func _process(delta: float) -> void:
 				if Rect2(i.global_position, i.size).has_point(get_global_mouse_position()):
 					inside_tab.x = int(i.get_index() % x_range)
 					inside_tab.y = int(i.get_index() / x_range)
+
+	current_tab_node.get_node("Items").position.y = move_toward(current_tab_node.get_node("Items").position.y, clamp(tab_data[tab].offset-(inside_tab.y-1)*tab_data[tab].size,tab_data[tab].size-tab_data[tab].size*(y_range-3), tab_data[tab].offset), 1600*delta)
+
 
 func _input(event: InputEvent) -> void:
 	if open:
