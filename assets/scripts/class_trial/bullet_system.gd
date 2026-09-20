66a392e3a648c05fde2f3e6844561c9a5ca3cfeb
Danilo <eggwardchickfila@gmail.com>
2026-09-20 03:42:57 +0200

    words camera data

--- assets/scripts/class_trial/bullet_system.gd
+++ assets/scripts/class_trial/bullet_system.gd
@@ -8,7 +8,6 @@ extends Control
 @onready var crosshair_anim: AnimationPlayer = $"../crosshair/crosshair_anim"
 @onready var downtime: Timer = $downtime
 @onready var camera: Node3D = $"../../CameraNode"
-@onready var word_bullet: Label3D = $word_bullet
 @onready var root: Control = $".."
 
 var bullet_direction = Vector3(0,0,0)
@@ -18,8 +17,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if downtime.time_left > 0.0:
-		word_bullet.global_position += bullet_direction * 10.0 * delta
+	pass
 		
 func truth_shoot():
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -34,13 +32,6 @@ func truth_shoot():
 	#var ray_origin = camera.get_node("ClassTrialCamera").project_ray_origin(mouse_pos)
 	var ray_direction = camera.get_node("ClassTrialCamera").project_ray_normal(mouse_pos)
 	bullet_direction = ray_direction.normalized()
-	word_bullet.visible = true
-	
-	word_bullet.rotation = camera.global_rotation
-	word_bullet.rotation.y = camera.global_rotation.y-1.5
-	word_bullet.global_position = camera.global_position
-	word_bullet.global_position += camera.global_transform.basis.x * 1
-	word_bullet.global_position -= -camera.global_transform.basis.z * 4
 	
 func white_noise_shoot():
 	crosshair_anim.stop()
@@ -55,4 +46,3 @@ func _on_downtime_timeout() -> void:
 	reload()
 func reload():
 	shoot_anim.play("reload")
-	word_bullet.visible = false
